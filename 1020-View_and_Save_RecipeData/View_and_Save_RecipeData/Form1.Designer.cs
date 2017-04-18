ec89e048935ae7061050334c8945bec8b887dd4c
Kevin Cheng <kevin.cheng@advantech.com.tw>
2017-04-18 17:56:28 +0800

    [modify] rename start button

--- 1020-View_and_Save_RecipeData/View_and_Save_RecipeData/Form1.Designer.cs
+++ 1020-View_and_Save_RecipeData/View_and_Save_RecipeData/Form1.Designer.cs
@@ -184,7 +184,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 98);
             this.button1.TabIndex = 21;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
